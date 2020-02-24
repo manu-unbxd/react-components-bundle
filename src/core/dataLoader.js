95ce080206821157d77b95c90811dda1fc675dc1
Anupama <anupama@Anupamas-MacBook-Pro.local>
2020-02-24 15:03:43 +0530

    Adding middleware support in dataLoader

--- src/core/dataLoader.js
+++ src/core/dataLoader.js
@@ -5,11 +5,12 @@ import utils from "./utils";
 class DataLoader {
     _requestsMap = {}
     _commonHeaders = {}
+    _middlewares = {}
     setCommonHeaders (headers) {
         this._commonHeaders = {...this._commonHeaders, ...headers};
     }
     setResponseParser () {
-    
+        
     }
     setRequestParser () {
 
@@ -17,11 +18,40 @@ class DataLoader {
     addRequestConfig (requestId, requestConfig) {
         this._requestsMap[requestId] = requestConfig;
     }
-    getRequestDef({ requestId, params = {}, headers = {} }) {
+    getRequestMiddleware (requestId) {
+        let requestMiddleware = (x, y) => y, 
+            middleware = this._middlewares[requestId];
+
+        if (typeof(middleware) !== "undefined" && typeof(middleware.reqParser) === "function") {
+            requestMiddleware = middleware.reqParser;
+        }
+
+        return requestMiddleware;
+    }
+    getResponseMiddleware (requestId) {
+        let responseMiddleware = (x, y) => y, 
+            middleware = this._middlewares[requestId];
+
+        if (typeof(middleware) !== "undefined" && typeof(middleware.resParser) === "function") {
+            responseMiddleware = middleware.resParser;
+        }
+
+        return responseMiddleware;
+    }
+    getRequestParams (requestId, params) {
+        const requestParser = this.getRequestMiddleware(requestId);
+        return requestParser(requestId, params);
+    }
+    parseResponseData (requestId, response) {
+        const responseParser = this.getResponseMiddleware(requestId);
+        return responseParser(requestId, response);
+    }
+    getRequestDef ({ requestId, params = {}, headers = {} }) {
         const requestConfig = this._requestsMap[requestId];
         const { url, method = "GET" } = requestConfig;
-
-        let requestUrl = (typeof(url) === "function") ? url(params) : url;
+        const finalRequestParams = this.getRequestParams(requestId, params);
+        
+        let requestUrl = (typeof(url) === "function") ? url(finalRequestParams) : url;
         let reqMethod = method.toLowerCase();
 
         let requestMetadata = {
@@ -31,13 +61,13 @@ class DataLoader {
 
 
         if (reqMethod === "get") {
-            requestUrl = `${requestUrl}?${utils.getQueryParams(params)}`;
+            requestUrl = `${requestUrl}?${utils.getQueryParams(finalRequestParams)}`;
         } else  if (["post", "delete", "put", "patch"].indexOf(reqMethod) > -1) {
-            requestMetadata.body = JSON.stringify(params);
+            requestMetadata.body = JSON.stringify(finalRequestParams);
         } else if (reqMethod === "form_post" || reqMethod === "upload") {
             const formData = new FormData();
-            for (const key in params) {
-                formData.append(key, params[key]);
+            for (const key in finalRequestParams) {
+                formData.append(key, finalRequestParams[key]);
             }
             requestMetadata.body = formData;
         }
@@ -45,13 +75,24 @@ class DataLoader {
         return new Promise((resolve, reject) => {
             return fetchPolyfill(requestUrl, requestMetadata)
                 .then(response => response.json())
-                .then(json =>{
-                    resolve(json);
+                .then(json => {
+                    const parsedResponse = this.parseResponseData(requestId, json);
+                    resolve(parsedResponse);
                 })
                 .catch(exception => {
                     reject(exception);
                 });
         });
+    }
+    addMiddleware (requestId, { requestParser, responseParser }) {
+        if (typeof(this._middlewares[requestId]) !== "undefined") {
+            throw new Error(`Middleware for ${requestId} already exists`);
+        }
+
+        this._middlewares[requestId] = {
+            reqParser: requestParser,
+            resParser: responseParser
+        };
     }
 }
 
--- src/core/dataLoader.stories.js
+++ src/core/dataLoader.stories.js
@@ -7,6 +7,20 @@ dataLoader.addRequestConfig("getUsers", {
     url: "https://jsonplaceholder.typicode.com/users",
 });
 
+dataLoader.addRequestConfig("getTodo", {
+    method: "GET",
+    url: function(params) {
+        return `https://jsonplaceholder.typicode.com/todos/${params.todoId}`
+    }
+});
+
+dataLoader.addRequestConfig("getPost", {
+    method: "GET",
+    url: function(params) {
+        return `https://jsonplaceholder.typicode.com/posts/${params.id}`
+    }
+});
+
 export const SimpleUsage = () => {
     const [ data, setData ] = useState([]);
 
@@ -28,6 +42,76 @@ export const SimpleUsage = () => {
         <p>Use the <code>dataLoader</code> instance to make explicit API calls. 
         This would mostly be required for create/update/delete calls like POST, PUT, PATCH or DELETE</p>
         {(data && data.length > 0) && <List items={data} />}
+    </div>);
+};
+
+export const RequestMiddleware = () => {
+    const [ data, setData ] = useState([]);
+    const { title } = data;
+
+    useEffect(() => {
+        dataLoader.addMiddleware("getTodo", {
+            requestParser: function(requestId, params) {
+                return {
+                    todoId: params.id
+                };
+            }
+        });
+
+        const def = dataLoader.getRequestDef({
+            requestId: "getTodo",
+            params: {
+                id: 1
+            }
+        });
+    
+        def.done((response) => {
+            setData(response);
+        });
+    
+        def.catch((e) => {
+            console.error(e);
+        });
+    }, []);
+
+    return (<div>
+        <p>Use the <code>addMiddleware</code> functionality to add request middleware code to parse/modify the request data before sending it to the API.</p>
+        <div>Todo name is {title}</div>
+    </div>);
+};
+
+export const ResponseMiddleware = () => {
+    const [ data, setData ] = useState([]);
+    const { postName } = data;
+
+    useEffect(() => {
+        dataLoader.addMiddleware("getPost", {
+            responseParser: function(requestId, response) {
+                return {
+                    postName: response.title
+                };
+            }
+        });
+
+        const def = dataLoader.getRequestDef({
+            requestId: "getPost",
+            params: {
+                id: 1
+            }
+        });
+    
+        def.done((response) => {
+            setData(response);
+        });
+    
+        def.catch((e) => {
+            console.error(e);
+        });
+    }, []);
+
+    return (<div>
+        <p>Use the <code>addMiddleware</code> functionality to add response middleware code to parse/modify the response data before using it in the component</p>
+        <div>Post name is {postName}</div>
     </div>);
 };
 
