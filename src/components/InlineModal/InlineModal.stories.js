e56a9e05cf922fe68ec127c31ba2b05b5fe743ce
Anupama <anupama@Anupamas-MacBook-Pro.local>
2020-05-18 21:35:29 +0530

    Adding checkbox InlineModal story

--- src/components/InlineModal/InlineModal.stories.js
+++ src/components/InlineModal/InlineModal.stories.js
@@ -1,9 +1,21 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import { linkTo } from "@storybook/addon-links";
 
 import { FRUITS_LIST } from "../../../public/Constants";
 import InlineModal, { InlineModalActivator, InlineModalBody } from "./InlineModal";
 import { List } from "../";
+
+/* eslint-disable react/prop-types */
+const CustomListItem = (props) => {
+    const { itemData } = props;
+    const { id, name } = itemData;
+
+    return (<li>
+        <input type="checkbox" id={id}></input>
+        <label htmlFor={id}>{name}</label>
+    </li>)
+};
+/* eslint-enable react/prop-types */
 
 export const SimpleUsage = () => {
     const onModalStateChange = (isModalOpen) => {
@@ -14,10 +26,10 @@ export const SimpleUsage = () => {
     return (<Fragment>
         <InlineModal onModalStateChange={onModalStateChange}>
             <InlineModalActivator>
-                <div>Select</div>
+                <div>Select a fruit</div>
             </InlineModalActivator>
             <InlineModalBody>
-                <List items={FRUITS_LIST} showApp={linkTo("List")} />
+                <List items={FRUITS_LIST} showApp={linkTo("List")} ListItem={CustomListItem} />
             </InlineModalBody>
         </InlineModal>
     </Fragment>);
