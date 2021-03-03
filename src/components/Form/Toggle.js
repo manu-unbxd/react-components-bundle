cdc2c570ad54ffde764da765d0c454ec40cf071c
Divya Daglia <dagliadivya [at] gmail.com>
2021-03-03 22:04:24 +0530

    chore(toggle):change active only when toggle prop 'value' changes

--- src/components/Form/Toggle.js
+++ src/components/Form/Toggle.js
@@ -11,7 +11,12 @@ const Toggle = (props) => {
     const { onValueChange } = useContext(FormContext);
 
     const toggleActive = () => {
-        setIsActive(!isActive);       
+        // setIsActive(!isActive);   
+        if (typeof(onChange) === "function") {
+            onChange(!isActive);
+        } else {
+            setIsActive(!isActive);
+        }   
     };
 
     const postFormValueChange = (value) => {
